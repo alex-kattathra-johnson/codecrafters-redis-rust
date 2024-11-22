3e18c7575fe9af98ea455e96c5935022a2286a6b
Alex Kattathra Johnson <alex.kattathra.johnson@gmail.com>
2024-11-22 16:37:00 -0600

    Allow multiple pings
    Signed-off-by: Alex Kattathra Johnson <alex.kattathra.johnson@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::Write, net::TcpListener};
+use std::{io::{Read, Write}, net::TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,7 +11,13 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write_all(b"+PONG\r\n").unwrap();
+                let mut buf = [0; 512];
+                loop {
+                    if stream.read(&mut buf).unwrap() == 0 {
+                        break
+                    }
+                    stream.write(b"+PONG\r\n").unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
